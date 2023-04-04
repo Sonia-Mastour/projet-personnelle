e50d9c3d12fe22c356571034950f04c3b63e7bd9
jelassikamel <kamel.jlassi@esprit.tn>
2023-04-04 15:01:16 +0200

    backend

--- sarou5/src/app/login/login.component.ts
+++ sarou5/src/app/login/login.component.ts
@@ -9,6 +9,54 @@ export class LoginComponent implements OnInit {
   constructor(private formBuilder: FormBuilder) { }
   
   ngOnInit(): void {
+
+    function login() {
+      const username = (document.getElementById("username") as HTMLInputElement).value;
+      const password = (document.getElementById("password") as HTMLInputElement).value;
+    
+      if (username === "myusername" && password === "mypassword") {
+        alert("Login successful!");
+      } else {
+        alert("Login failed. Please try again.");
+      }
+    }
+
+    function signup() {
+      const username = (document.getElementById("username") as HTMLInputElement).value;
+      const email = (document.getElementById("email") as HTMLInputElement).value;
+      const password = (document.getElementById("password") as HTMLInputElement).value;
+    
+      // Perform validation on the input fields
+      if (!username || !email || !password) {
+        alert("Please fill out all fields.");
+        return;
+      }
+    
+      // Perform additional validation on the email field
+      if (!isValidEmail(email)) {
+        alert("Please enter a valid email address.");
+        return;
+      }
+    
+      // Create a new user object and store it somewhere
+      const newUser = {
+        username: username,
+        email: email,
+        password: password
+      };
+      // Here you would typically store the new user object in a database or some other storage system
+    
+      // Display a success message to the user
+      alert("Sign-up successful!");
+    }
+    
+    function isValidEmail(email: string) {
+      // Perform email validation using a regular expression
+      const emailRegex = /\S+@\S+\.\S+/;
+      return emailRegex.test(email);
+    }
+    
+    
     
     throw new Error('Method not implemented.');
   }
